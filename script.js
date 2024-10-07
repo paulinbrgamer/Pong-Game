1dc187573faed24851f9bc76acedc158c31961e5
Paulo <pauloferadosgames@gmail.com>
2024-10-07 12:21:32 -0300

    Ajuste no algoritimo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { drawnRec } from "./func/drawn.js"
 var canvas = document.getElementById('display')
 var ctx = canvas.getContext('2d')
+var velocity = 10
 window.init = init
 window.reset =reset
 var keys = {}
@@ -59,77 +60,69 @@ function update(){
     }
     
     //colisao da bola p2
-    if( bol.x<= p2.x+p2.w&& bol.x>= p2.x && bol.y<= p2.y+p2.h-10&& bol.y>= p2.y-10){
+    if(bol.x== p2.x-p2.w && bol.y+bol.h >= p2.y&& bol.y <= p2.y+p2.h){
         bol.dx = -1
         bol.x += bol.a*bol.dx
         p2.beats++
         if(keys['ArrowLeft'] && p2.beats>=3){
-            bol.a = 20
-            p1.a = 20
-            p2.a = 20
             p2.beats = 0
-        }else{
-            bol.a = 10
-            p1.a = 10
-            p2.a = 10 
+            velocity *=3
+            
         }
+        console.log("Colidiu")
     }
     //colisao da bola p1
-    if(bol.x<= p1.x+p1.w&& bol.x>= p1.x&& bol.y<= p1.y+p1.h+10&& bol.y>= p1.y+10){
+    if(bol.x== p1.x+p1.w && bol.y+bol.h >= p1.y&& bol.y <= p1.y+p1.h){
         //mudar direção x
         bol.dx = 1
         bol.x += bol.a*bol.dx
         p1.beats++
         if(keys['d'] && p1.beats>=3){
-            bol.a = 20
-            p1.a = 20
-            p2.a = 20
             p1.beats = 0
-        }else {
-            bol.a = 10
-            p1.a = 10
-            p2.a = 10 
+            velocity *=3
+            
         }
+        console.log("Colidiu")  
     }
     //colisao paredes y
     if (bol.y >=canvas.height-20){
         bol.dy = -1
     }
-    if (bol.y <=20){
+    if (bol.y <=0){
         bol.dy = 1
     }
     
     if(bol.x>canvas.width){
-        bol.a = 0
-        p1.score++
-        p2.beats = 0
+        setTimeout(()=>{
         bol.x = 400
         bol.y = 150
-        bol.beats = 0
-        p1.a = 10
-        p2.a = 10
-        setTimeout(()=>{
-            bol.a = 10
-        },1000)
         
+        p2.beats = 0
+        },1000)
+        p1.score++
     }
     if(bol.x< 0){
-        bol.a = 0
-        p2.score++
-        p1.beats = 0
+        setTimeout(()=>{
         bol.x = 400
         bol.y = 150
-        bol.beats = 0
-        p1.a = 10
-        p2.a = 10
-        setTimeout(()=>{
-            bol.a = 10
+        
+        p1.beats = 0
+
         },1000)
+        p2.score++
     }
+    setTimeout(()=>{
+        bol.a = velocity
+    },2000)
     tail.x = bol.x
     tail.y = bol.y
     bol.x += bol.a*bol.dx
     bol.y += bol.a*bol.dy
+    if(velocity>10){
+        velocity--
+    }
+   
+    
 }
 function draw(){
     var sc = document.getElementById('score1')
@@ -152,14 +145,11 @@ function draw(){
     drawnRec(tail,ctx)
     setTimeout(()=>{
         requestAnimationFrame(draw)
-    },16,666)
+    },16.666)
 }
 function init(){
     document.getElementById('Reset').style.display = 'block'
     document.getElementById('start-button').style.display = 'none'
-    setTimeout(()=>{
-        bol.a = 10
-    },2000)
     draw()
     
 }
@@ -202,9 +192,6 @@ function reset(){
         h:20,
         color:'rgba(255,255,0,0.9)',
     }
-    setTimeout(()=>{
-        bol.a = 10
-    },1000)
 }
 //capiturar inputes
 window.addEventListener('keyup',(event)=>{
@@ -212,7 +199,9 @@ window.addEventListener('keyup',(event)=>{
 })
 window.addEventListener('keydown',(event)=>{
     if (event.key == 'Enter'){
-        init()
+        if (document.getElementById('start-button').style.display != 'none'){
+            init()
+        }
     }
     keys[event.key] = true
     
